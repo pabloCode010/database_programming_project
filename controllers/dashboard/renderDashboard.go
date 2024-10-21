f09d2d6230b2cf078fef0e6678168148fd6e04f6
Pablo <pablocode010@gmail.com>
2024-10-21 16:28:07 -0600

    feat: add dynamic titles on dashboard template and set in controller

--- controllers/dashboard/renderDashboard.go
+++ controllers/dashboard/renderDashboard.go
@@ -6,10 +6,17 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+var dashboardTitles = map[string]string{
+	"users":  "Panel de Usuarios",
+	"movies": "Panel de Películas",
+	"genres": "Panel de Géneros(Películas)",
+}
+
 func RenderDashboard(c echo.Context) error {
 	entity := c.Param("entity")
 
 	return c.Render(http.StatusOK, "dashboard.html", map[string]interface{}{
 		"Entity": entity,
+		"Title":  dashboardTitles[entity],
 	})
 }
